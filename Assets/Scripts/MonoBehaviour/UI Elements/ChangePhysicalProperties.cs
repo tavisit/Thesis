a6d8644b274d55c8e7b002805d55cd77e9098953
Matei Octavian <mateitavis@gmail.com>
2024-05-09 22:25:00 +0300

    Style fix

--- Assets/Scripts/MonoBehaviour/UI Elements/ChangePhysicalProperties.cs
+++ Assets/Scripts/MonoBehaviour/UI Elements/ChangePhysicalProperties.cs
@@ -4,18 +4,18 @@ using UnityEngine.UI;
 public class ChangePhysicalProperties : MonoBehaviour
 {
     [SerializeField]
-    private Image panelChangePhysicalProperties; 
+    private Image panelChangePhysicalProperties;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PanelSetVisibility()
