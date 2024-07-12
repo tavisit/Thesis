d52d7417c0163e98eb272c85dbed9f92f8403fd0
Matei Octavian <mateitavis@gmail.com>
2024-07-12 18:01:38 +0300

    Some modifications to the parsing

--- Assets/Scripts/MonoBehaviour/ManagerScripts/DataFetching.cs
+++ Assets/Scripts/MonoBehaviour/ManagerScripts/DataFetching.cs
@@ -33,24 +33,6 @@ public class DataFetching
             List<OpenClBodyObject> returnObjects = JsonHelper.FromJson<OpenClBodyObject>(jsonContent).ToList();
 
             FindMinMaxPositions(returnObjects);
-
-            // Add Sagittarius A*
-            // Actual mass in solar masses - 1.0179712045256999e+11f
-            // Observed mass in solar masses - 4.297E+6f
-            OpenClBodyObject sagittariusA = new OpenClBodyObject("Blackhole Sagittarius A*", 1.0179712045256999e+11f, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-            returnObjects.Add(sagittariusA);
-
-            // Add random blackholes in the galaxy
-            for (int i = 0; i < 20; i++)
-            {
-                float x = Random.Range(minPosition.x, maxPosition.x);
-                float y = Random.Range(minPosition.y, maxPosition.y);
-                float z = Random.Range(minPosition.z, maxPosition.z);
-
-                OpenClBodyObject blackHole = new OpenClBodyObject("Blackhole " + Random.Range(0, 9999).ToString(), Random.Range(3.0f, 10.0f), new Vector3(x, y, z), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-                returnObjects.Add(blackHole);
-            }
-
             return returnObjects;
         }
         else
@@ -76,14 +58,6 @@ public class DataFetching
             Debug.LogError("JSON file not found!");
             return new List<WikiPageObjList>();
         }
-    }
-
-    public List<OpenClBodyObject> SolarSystemFetching()
-    {
-        List<OpenClBodyObject> returnValues = new List<OpenClBodyObject>();
-        TextAsset[] clFiles = Resources.LoadAll<TextAsset>("InputManagement/output");
-        TextAsset mainFile = clFiles.FirstOrDefault(s => s.name.Contains("solar_system"));
-        return JsonHelper.FromJson<OpenClBodyObject>("{\"Items\":" + mainFile.text + "}").ToList();
     }
 
     private void FindMinMaxPositions(List<OpenClBodyObject> returnObjects)
